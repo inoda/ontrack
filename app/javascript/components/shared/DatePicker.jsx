f23d8d724515c33e51f7b1d6140ef624a3687e95
Isaac <isaacnoda@gmail.com>
2019-10-01 01:24:54 -0600

    Adds datepicker css as css file

--- app/javascript/components/shared/DatePicker.jsx
+++ app/javascript/components/shared/DatePicker.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 
 class Picker extends React.Component {
   constructor(props) {
