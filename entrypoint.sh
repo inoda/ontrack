a49268fd34409b4d6d563fecefb74cd36dd6d45d
Filipe Forattini <filipeforattini1@gmail.com>
2020-06-11 01:32:40 -0300

    refactor: removed unecessary scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-bundle install
-yarn install --check-files
-
-# bundle exec rake db:create
-# bundle exec rake db:migrate
-
-./bin/webpack \
-  & bundle exec rails server -b 0.0.0.0
+rails db:migrate
+./bin/webpack
+bundle exec rails server -b 0.0.0.0
