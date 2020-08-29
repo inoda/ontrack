36813afcc84a2440a4863e2d69023f9dc0869864
James Buckley <donquixote42@malteseplatypus.com>
2020-08-29 15:52:28 -0500

    allows setup scripts to use other ruby version managers

--- scripts.sh
+++ scripts.sh
@@ -1,37 +1,68 @@
 #!/bin/bash
-install_rails () {
-	echo "INSTALLING BUNDLER"
-	gem install bundler
+install_dependencies () {
+  if [[ $(which bundle) ]]
+  then
+    echo "INSTALLING GEMS"
+	  bundle install
+	  echo "INSTALLING JS PACKAGES"
+	  yarn install
+  else
+    echo "INSTALLING BUNDLER"
+    gem install bundler
+    echo "INSTALLING GEMS"
+	  bundle install
+	  echo "INSTALLING JS PACKAGES"
+	  yarn install
+  fi
+}
+
+setup_db () {
 	echo "CREATING DATABASE"
 	bundle exec rake db:create
+  echo "MIGRATING DATABASE"
+  bundle exec rake db:migrate
+}
+
+set_ruby_version () {
+  if [[ $(ruby -v) =~ "2.6.5" ]]
+  then
+    echo "You are on Ruby 2.6.5"
+  else
+    if [[ $(which rbenv) ]]
+    then
+      eval "$(rbenv init -)"
+    elif [[ $(which rvm) ]]
+    then
+      rvm 2.6.5
+    elif [[ $(which chruby) ]]
+    then
+      chruby $(cat .ruby-version)
+    fi
+  fi
+
 }
 
 start_server () {
 	echo "INITIALIZING RUBY VERSION MANAGER"
-	eval "$(rbenv init -)"
-	echo "INSTALLING GEMS"
-	bundle install
-	echo "INSTALLING JS PACKAGES"
-	yarn install
-	echo "ENSURING DATABASE IS MIGRATED"
-	bundle exec rake db:migrate
+	set_ruby_version
+	echo "INSTALLING DEPENDENCIES"
+  install_dependencies
+  echo "SETTING UP DATABASE"
+  setup_db
 	echo "STARTING SERVER AT PORT 3000"
 	bundle exec rails s
 }
 
-if [ "$1" == "start" ]
+if [[ "$1" == "start" ]]
 then
 	start_server
-fi
-
-if [ "$1" == "install_with_brew" ]
+elif [[ "$1" == "install_with_brew" ]]
 then
 	echo "INSTALLING RUBY VERSION MANAGER"
 	brew install rbenv ruby-build || (echo "brew install rbenv ruby-build failed - continuing")
 	echo "INITIALIZING RUBY VERSION MANAGER"
 	eval "$(rbenv init -)"
 	rbenv install 2.6.5 || (echo "ruby install failed - continuing")
-	install_rails
 	start_server
 fi
 
