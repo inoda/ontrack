3f4d25cff8b7fc36797b1bddbdd0eb4dd40c78bc
Isaac Noda <isaac.noda@gusto.com>
2021-09-04 00:36:04 -0500

    Makes ColorPicker swatches length constant

--- app/javascript/components/shared/ColorPicker.jsx
+++ app/javascript/components/shared/ColorPicker.jsx
@@ -1,6 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const QUICK_COLORS = [
+  '#899b9c',
+  '#88b779',
+  '#0d947a',
+  '#248ed5',
+  '#9053ad',
+  '#ffda7d',
+  '#f49119',
+  '#e64637',
+  '#dd5e8e',
+  '#ba3427',
+  '#b3b3b3',
+  '#c5e276',
+  '#0ab191',
+  '#95dce8',
+  '#c67171',
+  '#8e5b4c',
+  '#e9eeef',
+  '#9ad8a9',
+  '#c7f4f9',
+  '#d9c3ea',
+  '#fffacf',
+  '#ffcea6',
+  '#f9b6b6',
+  '#c7b299',
+  '#2d4053',
+  '#141516',
+  '#603813',
+];
+
 class ColorPicker extends React.Component {
   constructor(props) {
     super(props);
@@ -24,13 +54,13 @@ class ColorPicker extends React.Component {
   }
 
   colors() {
-    const allColors = ['#899b9c', '#88b779', '#0d947a', '#248ed5', '#9053ad', '#ffda7d', '#f49119', '#e64637', '#dd5e8e', '#ba3427', '#b3b3b3', '#c5e276', '#0ab191', '#95dce8', '#c67171', '#8e5b4c', '#e9eeef', '#9ad8a9', '#c7f4f9', '#d9c3ea', '#fffacf', '#ffcea6', '#f9b6b6', '#c7b299', '#2d4053', '#141516', '#603813'];
-
     const availColors = [];
     if (this.state.initialColor.length) { availColors.push(this.state.initialColor); }
-    for (const color of allColors) {
+    for (const color of QUICK_COLORS) {
       if (availColors.map((el) => el.toLowerCase()).includes(color.toLowerCase())) { continue; }
       if (this.props.omitColors.length && this.props.omitColors.map((el) => el.toLowerCase()).includes(color.toLowerCase())) { continue; }
+      if (availColors.length >= 15) { break; }
+
       availColors.push(color);
     }
 
@@ -54,7 +84,7 @@ class ColorPicker extends React.Component {
         {!this.state.useTextField && (
           <>
             {this.renderSwatches()}
-            <div className="mr-10">
+            <div className="mr-10 mt-5">
               <button onClick={this.useTextField} type="button" className="btn btn-sm btn-full">Enter hex instead</button>
             </div>
           </>
