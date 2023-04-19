dc446fa5ad5ca39862d8a07d8d96fce2718ae53c
Isaac Noda <isaacnoda@gmail.com>
2023-04-19 12:16:52 -0600

    Adds more category info in export

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -5,13 +5,13 @@ class ExportsController < ApplicationController
   end
 
   def create
-    headers = ['paid_at', 'amount_in_cents', 'category', 'description']
+    headers = ['paid_at', 'amount_in_cents', 'category', 'description', 'category_color']
 
     csv = CSV.generate(headers: true) do |csv|
       csv << headers
       Expense.includes(:category).find_in_batches do |batch|
         batch.each do |expense|
-          csv << [expense.paid_at, expense.amount, expense.category.name, expense.description]
+          csv << [expense.paid_at, expense.amount, expense.category.name, expense.description, expense.category.color]
         end
       end
     end
