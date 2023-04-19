d893142f61d08b3d1d06b29f9265cd3070037790
Isaac Noda <isaacnoda@gmail.com>
2023-04-19 12:57:25 -0600

    Adds rank to export

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -5,13 +5,13 @@ class ExportsController < ApplicationController
   end
 
   def create
-    headers = ['paid_at', 'amount_in_cents', 'category', 'description', 'category_color']
+    headers = ['paid_at', 'amount_in_cents', 'category', 'description', 'category_color', 'category_rank']
 
     csv = CSV.generate(headers: true) do |csv|
       csv << headers
       Expense.includes(:category).find_in_batches do |batch|
         batch.each do |expense|
-          csv << [expense.paid_at, expense.amount, expense.category.name, expense.description, expense.category.color]
+          csv << [expense.paid_at, expense.amount, expense.category.name, expense.description, expense.category.color, expense.category.rank]
         end
       end
     end
